42f821f8e9603b15e6e70b271142321b2940a1b9
Gummadi Avinash <avinashgummadi2021weblaunch@gmail.com>
2023-04-16 20:12:22 +0530

    api-modifications

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from apps.students.models import Student
+from apps.base.models import CustomUser
 from apps.staffs.models import Staff
 
 class StudentSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Student
-		fields ='__all__'
+    current_class_name = serializers.ReadOnlyField(source='current_class.name')
+    class Meta:
+        model = Student
+        fields = ['current_status', 'first_name', 'last_name', 'date_of_birth', 'gender', 'guardian_name', 'parent_mobile_number', 'date_of_admission', 'current_class_name', 'comments', 'passport']
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['chairman', 'principal', 'school_name', 'mobile_number', 'email', 'address', 'country']
   
 class StaffSerializer(serializers.ModelSerializer):
 	class Meta:
--- apps/api/views.py
+++ apps/api/views.py
@@ -3,10 +3,12 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import StudentSerializer,StaffSerializer
+from .serializers import StudentSerializer,StaffSerializer,CustomUserSerializer
 
 from apps.students.models import Student
+from apps.base.models import CustomUser
 from apps.staffs.models import Staff
+from rest_framework import status
 
 import hashlib
 
@@ -27,7 +29,7 @@ def staff(request,pk):
     task = Staff.objects.get(registration_number=pk)
     serialiser = StaffSerializer(task,many=False)
     return Response(serialiser.data)
-
+    
 @api_view(['GET'])
 def student(request):
     # Concatenate the words and encode as UTF-8
@@ -36,12 +38,39 @@ def student(request):
     hash_object = hashlib.sha256(text)
     # Convert the hash to a hexadecimal string
     token = hash_object.hexdigest()
-    student_id=request.query_params['studentid']
-    task = Student.objects.get(registration_number=student_id)
-    modid = request.query_params['modid']
-    serialiser = StudentSerializer(task,many=False)
-    paramstoken=request.query_params['token']
+    
+    # Check if studentid is present in query params
+    student_id = request.query_params.get('studentid')
+    if not student_id:
+        return Response({'error': 'Missing required parameter(studentid)', 'status': status.HTTP_422_UNPROCESSABLE_ENTITY}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    # Check if registerid is present in query params
+    register_id = request.query_params.get('registerid')
+    if not register_id:
+        return Response({'error': 'Missing required parameter(registerid)', 'status': status.HTTP_422_UNPROCESSABLE_ENTITY}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    # Check if modid is present in query params
+    modid = request.query_params.get('modid')
+    if not modid:
+        return Response({'error': 'Missing required parameter(modid)', 'status': status.HTTP_422_UNPROCESSABLE_ENTITY}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    # Check if token is present in query params
+    paramstoken = request.query_params.get('token')
+    if not paramstoken:
+        return Response({'error': 'Missing required parameter(token)', 'status': status.HTTP_422_UNPROCESSABLE_ENTITY}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
     if token == paramstoken and modid in MODLIST:
-        return Response(serialiser.data)
+        try:
+            userdata = CustomUser.objects.get(register_id=register_id)
+            userserializer = CustomUserSerializer(userdata, many=False)
+        except CustomUser.DoesNotExist:
+            return Response({'error': 'School not found', 'status': status.HTTP_404_NOT_FOUND}, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            studentdata = Student.objects.get(user=userdata, registration_number=student_id)
+            studentserializer = StudentSerializer(studentdata, many=False)
+            return Response({'status':status.HTTP_200_OK,'schooldata': userserializer.data, 'studentdata': studentserializer.data})
+        except Student.DoesNotExist:
+            return Response({'error': 'Student not found', 'status': status.HTTP_404_NOT_FOUND}, status=status.HTTP_404_NOT_FOUND)
     else:
-        return render(request,"404.html")
+        return Response({'error': 'Invalid parameter value', 'status': status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
