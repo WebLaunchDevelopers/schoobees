63afa7208144df3a383c67e5f8b7f5eb34d57582
Gummadi Avinash <avinashgummadi2021weblaunch@gmail.com>
2023-04-30 21:40:07 +0530

    huge3 staff changes

--- apps/staffs/views.py
+++ apps/staffs/views.py
@@ -5,9 +5,9 @@ from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from apps.base.models import CustomUser
+from django.contrib import messages
 
 from .models import Staff
-
 
 class StaffListView(ListView):
     model = Staff
@@ -44,8 +44,6 @@ class StaffCreateView(SuccessMessageMixin, CreateView):
 
         return super().form_valid(form)
 
-
-
 class StaffUpdateView(SuccessMessageMixin, UpdateView):
     model = Staff
     fields = ['current_status','first_name','last_name','gender','date_of_birth','email','mobile_number','address','comments']
@@ -59,6 +57,33 @@ class StaffUpdateView(SuccessMessageMixin, UpdateView):
         form.fields["comments"].widget = widgets.Textarea(attrs={"rows": 1})
         return form
 
+    def form_valid(self, form):
+        # Get the old Staff object
+        old_staff = self.get_object()
+
+        # Check if email has changed
+        old_email = old_staff.email
+        new_email = form.cleaned_data['email']
+        if old_email != new_email:
+            # Update email in related CustomUser object
+            related_user = CustomUser.objects.get(email=old_email)
+            related_user.email = new_email
+            related_user.save()
+
+        return super().form_valid(form)
+
 class StaffDeleteView(DeleteView):
     model = Staff
     success_url = reverse_lazy("staff-list")
+
+    def form_valid(self, form):
+        # Get the related CustomUser object
+        related_user = CustomUser.objects.get(email=self.object.email)
+
+        # Delete the related CustomUser object
+        related_user.delete()
+
+        messages.success(self.request, "Staff successfully deleted.")
+        return super().form_valid(form)
+
+
