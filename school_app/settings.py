6538e07b5d0ebf3212df94014465614c1d11de4b
DanielDominicSavioKennedy <saviodomnic2002@gmail.com>
2023-05-03 19:45:02 +0530

    new 404 page design

--- school_app/settings.py
+++ school_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # For 404 page keep it in False
+DEBUG = False # For 404 page keep it in False
 
 ALLOWED_HOSTS = ['*']
 
