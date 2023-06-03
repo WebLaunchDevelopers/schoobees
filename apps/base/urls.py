ac6f6673b53cb0000e5a9b97759f0307c646bc58
Gummadi Avinash <avinashgummadi2021weblaunch@gmail.com>
2023-06-04 01:56:20 +0530

    resolved all bugs

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -6,9 +6,8 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('password-reset/', views.PasswordResetView.as_view(), name='password_reset'),
-    path('password-reset/done/', views.PasswordResetView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset/complete/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('change-password/', views.ChangePasswordView.as_view(), name='change_password'),
     # Add other views and URLs here as needed
 ]
--- apps/base/utils.py
+++ apps/base/utils.py
@@ -5,12 +5,15 @@ from django.core.mail import send_mail
 from django.urls import reverse
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
 
 def generate_token():
     return secrets.token_urlsafe(20)
 
 def send_password_reset_email(user, request):
-    token = generate_token()
+    uidb64 = urlsafe_base64_encode(str(user.id).encode("utf-8"))
+
+    token = default_token_generator.make_token(user)
     user.reset_password_token = token
     user.reset_password_token_created_at = timezone.now()
     user.save()
@@ -19,7 +22,7 @@ def send_password_reset_email(user, request):
     domain = current_site.domain
     environment = 'http' if domain == '127.0.0.1:8000' else 'https'
 
-    reset_password_link = f"{environment}://{domain}/accounts/password-reset/{token}"
+    reset_password_link = f"{environment}://{domain}/accounts/password-reset/{uidb64}/{token}"
     send_mail(
         'Reset your password',
         f'Please click the following link to reset your password: {reset_password_link}',
@@ -27,9 +30,3 @@ def send_password_reset_email(user, request):
         [user.email],
         fail_silently=False,
     )
-
-def reset_password(user, new_password):
-    user.set_password(new_password)
-    user.reset_password_token = None
-    user.reset_password_token_created_at = None
-    user.save()
